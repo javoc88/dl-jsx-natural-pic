adb438c6b0ee630773ed448ef5f30e6f60a332d8
Javoc <javier.ochoa.77@gmail.com>
2023-09-21 17:11:07 -0300

    fix: se modifica navbar a formato de material complementario

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,10 @@
-import { Container, Nav, Navbar, Button } from "react-bootstrap";
+import "../assets/css/navbar.css";
 import { Link } from "react-router-dom";
 
 export default function navbar() {
   return (
-    <Navbar bg="secondary">
-      <Container>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mx-auto">
-            <Link to="/" className="text-decoration-none text-white me-5">
-              <Button variant="light"> Home </Button>
-            </Link>
-            <Link to="/favoritos" className="text-decoration-none text-white">
-              <Button variant="dark"> Favoritos </Button>
-            </Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <nav className="navbar">
+      <Link to="/"> Home </Link> | <Link to="/favoritos"> Favoritos </Link>
+    </nav>
   );
 }
