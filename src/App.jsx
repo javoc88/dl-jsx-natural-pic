76f84e354ae103f7ae425454c59bf4bb1ac630f1
Javoc <javier.ochoa.77@gmail.com>
2023-09-21 18:01:55 -0300

    feat: se crea contexto

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,28 @@ import Navbar from "./components/Navbar";
 import Home from "./views/Home";
 import Favoritos from "./views/Favoritos";
 import NotFound from "./views/NotFound";
+import { useContext } from "react";
+import { ContextAPI } from "./context/ContextAPI";
 
 export default function App() {
   const API_KEY = "2arMZRqLqedpt0T9aIApYZlw3yhepKtNaDDNyQ4txBDAvlKgCbv4HTkP";
+  const { dataImg, setDataImg } = useContext(ContextAPI);
+  const sharedData = { dataImg, setDataImg }
   const endpoint = "/fotos.json";
 
   return (
     <>
       <div className="App text-light">
-        <BrowserRouter>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/favoritos" element={<Favoritos />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+        <ContextAPI.Provider value={ sharedData }>
+          <BrowserRouter>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/favoritos" element={<Favoritos />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </ContextAPI.Provider>
       </div>
     </>
   );
